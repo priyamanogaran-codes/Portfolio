496670538f31e563b4305ac0b4601a9505e4fe24
Priya Dharshini <priyamanogaran445@gmail.com>
2026-07-14 15:49:57 +0530

    Fixed GitHub profile link

--- script.js
+++ script.js
@@ -70,3 +70,30 @@ AOS.init({
 
 });
 
+/*==============================
+    DARK / LIGHT MODE
+==============================*/
+
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("light-mode");
+
+    const icon = themeToggle.querySelector("i");
+
+    if(document.body.classList.contains("light-mode")){
+
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+
+    }
+
+    else{
+
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+
+    }
+
+});
